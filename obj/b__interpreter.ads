eacfa589703a1e85785aa37678977acf946f37f8
andrea <andreapinardi39@gmail.com>
2018-10-31 17:34:33 -0400

    first commit

---
+++ obj/b__interpreter.ads
@@ -0,0 +1,203 @@
+pragma Warnings (Off);
+pragma Ada_95;
+with System;
+with System.Parameters;
+with System.Secondary_Stack;
+package ada_main is
+
+   gnat_argc : Integer;
+   gnat_argv : System.Address;
+   gnat_envp : System.Address;
+
+   pragma Import (C, gnat_argc);
+   pragma Import (C, gnat_argv);
+   pragma Import (C, gnat_envp);
+
+   gnat_exit_status : Integer;
+   pragma Import (C, gnat_exit_status);
+
+   GNAT_Version : constant String :=
+                    "GNAT Version: Community 2018 (20180523-73)" & ASCII.NUL;
+   pragma Export (C, GNAT_Version, "__gnat_version");
+
+   Ada_Main_Program_Name : constant String := "_ada_interpreter" & ASCII.NUL;
+   pragma Export (C, Ada_Main_Program_Name, "__gnat_ada_main_program_name");
+
+   procedure adainit;
+   pragma Export (C, adainit, "adainit");
+
+   procedure adafinal;
+   pragma Export (C, adafinal, "adafinal");
+
+   function main
+     (argc : Integer;
+      argv : System.Address;
+      envp : System.Address)
+      return Integer;
+   pragma Export (C, main, "main");
+
+   type Version_32 is mod 2 ** 32;
+   u00001 : constant Version_32 := 16#f41b08ea#;
+   pragma Export (C, u00001, "interpreterB");
+   u00002 : constant Version_32 := 16#050ff2f0#;
+   pragma Export (C, u00002, "system__standard_libraryB");
+   u00003 : constant Version_32 := 16#0f7d71d4#;
+   pragma Export (C, u00003, "system__standard_libraryS");
+   u00004 : constant Version_32 := 16#085b6ffb#;
+   pragma Export (C, u00004, "systemS");
+   u00005 : constant Version_32 := 16#2323a8af#;
+   pragma Export (C, u00005, "system__memoryB");
+   u00006 : constant Version_32 := 16#512609cf#;
+   pragma Export (C, u00006, "system__memoryS");
+   u00007 : constant Version_32 := 16#f8088b52#;
+   pragma Export (C, u00007, "ada__exceptionsB");
+   u00008 : constant Version_32 := 16#16307b94#;
+   pragma Export (C, u00008, "ada__exceptionsS");
+   u00009 : constant Version_32 := 16#76789da1#;
+   pragma Export (C, u00009, "adaS");
+   u00010 : constant Version_32 := 16#5726abed#;
+   pragma Export (C, u00010, "ada__exceptions__last_chance_handlerB");
+   u00011 : constant Version_32 := 16#41e5552e#;
+   pragma Export (C, u00011, "ada__exceptions__last_chance_handlerS");
+   u00012 : constant Version_32 := 16#ae860117#;
+   pragma Export (C, u00012, "system__soft_linksB");
+   u00013 : constant Version_32 := 16#4d58644d#;
+   pragma Export (C, u00013, "system__soft_linksS");
+   u00014 : constant Version_32 := 16#bd45c2cc#;
+   pragma Export (C, u00014, "system__secondary_stackB");
+   u00015 : constant Version_32 := 16#4dcf97e2#;
+   pragma Export (C, u00015, "system__secondary_stackS");
+   u00016 : constant Version_32 := 16#86dbf443#;
+   pragma Export (C, u00016, "system__parametersB");
+   u00017 : constant Version_32 := 16#40b73bd0#;
+   pragma Export (C, u00017, "system__parametersS");
+   u00018 : constant Version_32 := 16#ced09590#;
+   pragma Export (C, u00018, "system__storage_elementsB");
+   u00019 : constant Version_32 := 16#259825ff#;
+   pragma Export (C, u00019, "system__storage_elementsS");
+   u00020 : constant Version_32 := 16#75bf515c#;
+   pragma Export (C, u00020, "system__soft_links__initializeB");
+   u00021 : constant Version_32 := 16#5697fc2b#;
+   pragma Export (C, u00021, "system__soft_links__initializeS");
+   u00022 : constant Version_32 := 16#41837d1e#;
+   pragma Export (C, u00022, "system__stack_checkingB");
+   u00023 : constant Version_32 := 16#86e40413#;
+   pragma Export (C, u00023, "system__stack_checkingS");
+   u00024 : constant Version_32 := 16#34742901#;
+   pragma Export (C, u00024, "system__exception_tableB");
+   u00025 : constant Version_32 := 16#55f506b9#;
+   pragma Export (C, u00025, "system__exception_tableS");
+   u00026 : constant Version_32 := 16#ce4af020#;
+   pragma Export (C, u00026, "system__exceptionsB");
+   u00027 : constant Version_32 := 16#6038020d#;
+   pragma Export (C, u00027, "system__exceptionsS");
+   u00028 : constant Version_32 := 16#80916427#;
+   pragma Export (C, u00028, "system__exceptions__machineB");
+   u00029 : constant Version_32 := 16#3bad9081#;
+   pragma Export (C, u00029, "system__exceptions__machineS");
+   u00030 : constant Version_32 := 16#aa0563fc#;
+   pragma Export (C, u00030, "system__exceptions_debugB");
+   u00031 : constant Version_32 := 16#76d1963f#;
+   pragma Export (C, u00031, "system__exceptions_debugS");
+   u00032 : constant Version_32 := 16#6c2f8802#;
+   pragma Export (C, u00032, "system__img_intB");
+   u00033 : constant Version_32 := 16#0a808f39#;
+   pragma Export (C, u00033, "system__img_intS");
+   u00034 : constant Version_32 := 16#39df8c17#;
+   pragma Export (C, u00034, "system__tracebackB");
+   u00035 : constant Version_32 := 16#5679b13f#;
+   pragma Export (C, u00035, "system__tracebackS");
+   u00036 : constant Version_32 := 16#9ed49525#;
+   pragma Export (C, u00036, "system__traceback_entriesB");
+   u00037 : constant Version_32 := 16#0800998b#;
+   pragma Export (C, u00037, "system__traceback_entriesS");
+   u00038 : constant Version_32 := 16#bb296fbb#;
+   pragma Export (C, u00038, "system__traceback__symbolicB");
+   u00039 : constant Version_32 := 16#c84061d1#;
+   pragma Export (C, u00039, "system__traceback__symbolicS");
+   u00040 : constant Version_32 := 16#701f9d88#;
+   pragma Export (C, u00040, "ada__exceptions__tracebackB");
+   u00041 : constant Version_32 := 16#20245e75#;
+   pragma Export (C, u00041, "ada__exceptions__tracebackS");
+   u00042 : constant Version_32 := 16#a0d3d22b#;
+   pragma Export (C, u00042, "system__address_imageB");
+   u00043 : constant Version_32 := 16#a9b7f2c1#;
+   pragma Export (C, u00043, "system__address_imageS");
+   u00044 : constant Version_32 := 16#8c33a517#;
+   pragma Export (C, u00044, "system__wch_conB");
+   u00045 : constant Version_32 := 16#13264d29#;
+   pragma Export (C, u00045, "system__wch_conS");
+   u00046 : constant Version_32 := 16#9721e840#;
+   pragma Export (C, u00046, "system__wch_stwB");
+   u00047 : constant Version_32 := 16#3e376128#;
+   pragma Export (C, u00047, "system__wch_stwS");
+   u00048 : constant Version_32 := 16#a831679c#;
+   pragma Export (C, u00048, "system__wch_cnvB");
+   u00049 : constant Version_32 := 16#1c91f7da#;
+   pragma Export (C, u00049, "system__wch_cnvS");
+   u00050 : constant Version_32 := 16#5ab55268#;
+   pragma Export (C, u00050, "interfacesS");
+   u00051 : constant Version_32 := 16#ece6fdb6#;
+   pragma Export (C, u00051, "system__wch_jisB");
+   u00052 : constant Version_32 := 16#9ce1eefb#;
+   pragma Export (C, u00052, "system__wch_jisS");
+   u00053 : constant Version_32 := 16#4e0ce0a1#;
+   pragma Export (C, u00053, "system__crtlS");
+
+   --  BEGIN ELABORATION ORDER
+   --  ada%s
+   --  interfaces%s
+   --  system%s
+   --  system.img_int%s
+   --  system.img_int%b
+   --  system.parameters%s
+   --  system.parameters%b
+   --  system.crtl%s
+   --  system.storage_elements%s
+   --  system.storage_elements%b
+   --  system.stack_checking%s
+   --  system.stack_checking%b
+   --  system.traceback_entries%s
+   --  system.traceback_entries%b
+   --  system.wch_con%s
+   --  system.wch_con%b
+   --  system.wch_jis%s
+   --  system.wch_jis%b
+   --  system.wch_cnv%s
+   --  system.wch_cnv%b
+   --  system.traceback%s
+   --  system.traceback%b
+   --  system.wch_stw%s
+   --  system.standard_library%s
+   --  system.exceptions_debug%s
+   --  system.exceptions_debug%b
+   --  ada.exceptions%s
+   --  system.wch_stw%b
+   --  ada.exceptions.traceback%s
+   --  system.secondary_stack%s
+   --  system.address_image%s
+   --  system.soft_links%s
+   --  system.exception_table%s
+   --  system.exception_table%b
+   --  system.exceptions%s
+   --  system.exceptions%b
+   --  ada.exceptions.last_chance_handler%s
+   --  system.memory%s
+   --  system.memory%b
+   --  system.standard_library%b
+   --  ada.exceptions.traceback%b
+   --  system.traceback.symbolic%s
+   --  system.traceback.symbolic%b
+   --  system.exceptions.machine%s
+   --  system.exceptions.machine%b
+   --  system.secondary_stack%b
+   --  system.address_image%b
+   --  system.soft_links.initialize%s
+   --  system.soft_links.initialize%b
+   --  system.soft_links%b
+   --  ada.exceptions.last_chance_handler%b
+   --  ada.exceptions%b
+   --  interpreter%b
+   --  END ELABORATION ORDER
+
+end ada_main;
